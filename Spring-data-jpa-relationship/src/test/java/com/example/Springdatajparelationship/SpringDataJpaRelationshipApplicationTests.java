c28fc521daa926b14ccf5f06885548f0c4011b4e
heng somnang <134189464+Nangkiller098@users.noreply.github.com>
2023-06-03 11:43:48 +0700

    Initial commit

---
+++ Spring-data-jpa-relationship/src/test/java/com/example/Springdatajparelationship/SpringDataJpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springdatajparelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
